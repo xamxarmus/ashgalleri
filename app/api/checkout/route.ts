e95d850c511907cce780ac6c2c2e884459ccee73
xamxarmus <azrilshafwan125@gmail.com>
2026-09-11 19:35:10 +0000

    Kemas kini Stripe Dahlia

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
-// Mengaktifkan enjin Stripe menggunakan Kunci Rahsia awak
+// Mengaktifkan enjin Stripe menggunakan Kunci Rahsia awak berserta versi "dahlia" 🌺
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-06-20',
+  apiVersion: '2026-08-26.dahlia', 
 });
 
 export async function POST(request: Request) {
